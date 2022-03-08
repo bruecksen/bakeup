301793930b3784bb1df248bff963c9d263491687
Matthias Brück <hi@brueck.io>
2022-03-08 20:25:55 +0100

    add whitenoise again

--- config/settings/production.py
+++ config/settings/production.py
@@ -63,7 +63,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 
