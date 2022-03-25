0fb15c2efa02916e289c8f75c4b4af99ae6d992c
NIels Göttsch <niels@ziemlichoptimal.de>
2022-03-25 09:17:05 +0100

    Fixing double admin url

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path("workshop/", include("bakeup.workshop.urls", namespace="workshop")),
     path("login/", auth_views.LoginView.as_view(), name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name='logout'),
-    path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
