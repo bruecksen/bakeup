7d4b53469b72dce0ef07bf554c6f6d9e43bb5e90
NIels Göttsch <niels@ziemlichoptimal.de>
2022-05-11 09:53:29 +0200

    add customerorder an Productiondays to fixturescript and update fixtures

--- build-demo-fixtures.sh
+++ build-demo-fixtures.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 python manage.py dumpdata workshop.Product workshop.ProductHierarchy workshop.Instruction> bakeup/workshop/fixtures/demo-products.json
+python manage.py dumpdata shop.CustomerOrder shop.CustomerOrderPosition shop.ProductionDay shop.ProductionDayProduct workshop.ProductionPlan > bakeup/shop/fixtures/demo_baking_days.json
