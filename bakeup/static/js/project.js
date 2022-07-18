5fdec29d5f434476def687e08b23ef977bb58e5e
Matthias Brück <hi@brueck.io>
2022-07-18 21:08:47 +0200

    Add remove button

--- bakeup/static/js/project.js
+++ bakeup/static/js/project.js
@@ -3,18 +3,45 @@ let container = document.querySelector(".form-add-inline");
 let addAnotherContainer = document.querySelector(".add-another-container");
 let addButton = document.querySelector(".add-another-form");
 let totalForms = document.querySelector("#id_form-TOTAL_FORMS");
+let saveButton = document.querySelector(".save-another-form");
 addButton.addEventListener('click', addForm);
+
+function addRemoveButtonEvent(removeButtons) {
+    for (let i = 0; i < removeButtons.length; i++) {
+        removeButtons[i].addEventListener("click", removeForm);
+    }
+}
+
 
 function addForm(e){
     e.preventDefault();
-    
+    saveButton.classList.remove('d-none');
     let totalFormValue = parseInt(totalForms.value);
     let newForm = form[0].cloneNode(true);
     let formRegex = RegExp(`form-(\\d){1}-`,'g');
 
     newForm.classList.remove('d-none');
     newForm.innerHTML = newForm.innerHTML.replace(/__prefix__/g, `${totalFormValue}`);
+    console.log(form[0])
     container.insertBefore(newForm, form[0]);
     
     totalForms.setAttribute('value', `${totalFormValue+1}`);
+    let removeButtons = document.querySelectorAll(".remove-another-form");
+    addRemoveButtonEvent(removeButtons);
+    
+}
+
+let removeButtons = document.querySelectorAll(".remove-another-form");
+
+addRemoveButtonEvent(removeButtons);
+
+function removeForm(e) {
+    console.log('click');
+    e.preventDefault();
+    e.target.closest('.list-group-item').remove();
+    let totalFormValue = parseInt(document.querySelector("#id_form-TOTAL_FORMS").value);
+    totalForms.setAttribute('value', `${totalFormValue-1}`);
+    if (totalFormValue - 1 == 0) {
+        saveButton.classList.add('d-none');
+    }
 }
