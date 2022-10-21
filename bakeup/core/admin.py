15ae025866aee107be89ac8070f08026ef1ee0f1
Matthias Brück <hi@brueck.io>
2022-10-21 12:03:09 +0200

    Show domain in admin

--- bakeup/core/admin.py
+++ bakeup/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from django_tenants.admin import TenantAdminMixin
 
-from bakeup.core.models import Client
+from bakeup.core.models import Client, Domain
 
 # Register your models here.
 
@@ -14,3 +14,8 @@ class ExcludeAdminMixin(object):
 @admin.register(Client)
 class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
         list_display = ('name', 'created')
+
+
+@admin.register(Domain)
+class ClientAdmin(admin.ModelAdmin):
+        list_display = ('domain', 'tenant', 'is_primary')
