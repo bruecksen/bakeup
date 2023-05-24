71534e12023fcef0286368ce056e9d39035c564c
Matthias Brück <hi@brueck.io>
2023-05-24 14:42:39 +0200

    Hide items from wagtail menus

---
+++ bakeup/pages/wagtail_hooks.py
@@ -0,0 +1,12 @@
+from wagtail.core import hooks
+
+@hooks.register("construct_settings_menu")
+def hide_user_menu_item(request, menu_items):
+    exclude_item_list = ('benutzer', 'gruppen', 'users', 'groups', 'workflows', 'workflow-tasks')
+    menu_items[:] = [item for item in menu_items if item.name not in exclude_item_list]
+
+
+@hooks.register('construct_main_menu')
+def hide_snippets_menu_item(request, menu_items):
+    exclude_item_list = ('berichte', 'reports')
+    menu_items[:] = [item for item in menu_items if item.name not in exclude_item_list]
