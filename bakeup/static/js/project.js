7269789a22ce55ce2a32c25a358a1f1b81cb7da2
Matthias Brück <hi@brueck.io>
2023-06-21 15:44:51 +0200

    Add toast as alert messages, fix cart if user is not logged in

--- bakeup/static/js/project.js
+++ bakeup/static/js/project.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function(toastEl) {
+    // Creates an array of toasts (it only initializes them)
+        return new bootstrap.Toast(toastEl) // No need for options; use the default options
+    });
+    console.log(toastList);
+    toastList.forEach(toast => toast.show()); // This show them
     if (location.hash) {
         $("button[data-bs-target='" + location.hash + "']").tab("show");
     }
