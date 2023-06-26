5442dd05d4dc3ca9e0e13737c803183650d04545
Matthias Brück <hi@brueck.io>
2023-06-26 18:04:45 +0200

    Add missing url

--- bakeup/shop/urls.py
+++ bakeup/shop/urls.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 from wagtail import urls as wagtail_urls
 
-from bakeup.shop.views import customer_order_add_or_update, ProductionDayListView, redirect_to_production_day_view, CustomerOrderUpdateView, CustomerOrderAddView, ProductListView, ShopView, ProductionDayWeeklyView, CustomerOrderPositionDeleteView, CustomerOrderPositionUpdateView
+from bakeup.shop.views import customer_order_add_or_update, CustomerOrderListView, ProductionDayListView, redirect_to_production_day_view, CustomerOrderUpdateView, CustomerOrderAddView, ProductListView, ShopView, ProductionDayWeeklyView, CustomerOrderPositionDeleteView, CustomerOrderPositionUpdateView
 from bakeup.users.views import LoginView, TokenLoginView, SignupView
 from bakeup.users.views import (
     shop_user_profile_view,
@@ -27,6 +27,7 @@ urlpatterns = [
    path("orders/<int:pk>/update/", view=CustomerOrderUpdateView.as_view(), name="customer-order-update"),
    path("orders/positions/<int:pk>/delete/", view=CustomerOrderPositionDeleteView.as_view(), name="customer-order-position-delete"),
    path("orders/positions/<int:pk>/update/", view=CustomerOrderPositionUpdateView.as_view(), name="customer-order-position-update"),
+   path(_("orders/"), view=CustomerOrderListView.as_view(), name="order-list"),
    path("profile/", view=shop_user_update_view, name="user-profile"),
     path(_("login/"), view=LoginView.as_view(), name="login"),
     path(_("signup/"), view=SignupView.as_view(), name="signup"),
