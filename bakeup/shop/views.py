d572729bb014a3517a07e4693670de22604860f6
Matthias Brück <hi@brueck.io>
2023-07-04 14:12:08 +0200

    Show abo count in order list

--- bakeup/shop/views.py
+++ bakeup/shop/views.py
@@ -163,7 +163,7 @@ class CustomerOrderListView(CustomerRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['point_of_sales'] = PointOfSale.objects.all()
-        context['product_abos'] = list(CustomerOrderTemplatePosition.objects.active().filter(order_template__customer=self.request.user.customer).values_list('product', flat=True))
+        context['product_abos'] = {abo['product']: abo['quantity'] for abo in CustomerOrderTemplatePosition.objects.active().filter(order_template__customer=self.request.user.customer).values('product', 'quantity')}
         return context
 
     def get_queryset(self):
