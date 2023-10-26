ad10c79e28c506b7b56b6750a90af5e3e1793230
Matthias Brück <hi@brueck.io>
2023-10-26 15:39:13 +0200

    Fix using default settings if no client settings provided

--- bakeup/core/tenant_settings.py
+++ bakeup/core/tenant_settings.py
@@ -1,28 +1,33 @@
 from django.conf import settings
 
+from config.settings.base import env
+
 
 class TenantSettings:
     @classmethod
     def overload_settings(cls, tenant, request=None):
         if hasattr(tenant, "clientsetting"):
-            print("loading client settings")
             client_settings = tenant.clientsetting
             if client_settings.default_from_email:
                 settings.DEFAULT_FROM_EMAIL = client_settings.default_from_email
                 settings.SERVER_EMAIL = client_settings.default_from_email
-            if client_settings.email_host:
                 settings.EMAIL_HOST = client_settings.email_host
-            if client_settings.email_host_password:
                 settings.EMAIL_HOST_PASSWORD = client_settings.email_host_password
-            if client_settings.email_host_user:
                 settings.EMAIL_HOST_USER = client_settings.email_host_user
-            if client_settings.email_port:
                 settings.EMAIL_PORT = client_settings.email_port
-            if client_settings.email_use_tls:
                 settings.EMAIL_USE_TLS = client_settings.email_use_tls
+            else:
+                settings.EMAIL_HOST = env("DJANGO_EMAIL_HOST", default=None)
+                settings.EMAIL_HOST_PASSWORD = env(
+                    "DJANGO_EMAIL_HOST_PASSWORD", default=None
+                )
+                settings.EMAIL_HOST_USER = env("DJANGO_EMAIL_HOST_USER", default=None)
+                settings.EMAIL_PORT = env("DJANGO_EMAIL_PORT", default=587)
+                settings.EMAIL_USE_TLS = env("DJANGO_EMAIL_USE_TLS", default=True)
             if client_settings.account_email_verification:
                 settings.ACCOUNT_EMAIL_VERIFICATION = (
                     client_settings.account_email_verification
                 )
+
         if request:
             settings.WAGTAILADMIN_BASE_URL = tenant.get_absolute_primary_domain(request)
