87efcfedadf08abfb0f966136a011bb99159e0a9
Matthias Brück <hi@brueck.io>
2024-03-04 12:03:05 +1100

    Add product price to fixtures

--- build-demo-fixtures.sh
+++ build-demo-fixtures.sh
@@ -2,4 +2,4 @@
 python manage.py tenant_command dumpdata --all --schema=localhost shop.PointOfSale > bakeup/shop/fixtures/demo_point_of_sale.json
 python manage.py tenant_command dumpdata --all --schema=localhost users auth shop.Customer > bakeup/users/fixtures/demo_users.json
 python manage.py tenant_command dumpdata --all --schema=localhost workshop.Category > bakeup/workshop/fixtures/demo_categories.json
-python manage.py tenant_command dumpdata --all --schema=localhost workshop.Product workshop.ProductHierarchy workshop.Instruction taggit.Tag taggit.TaggedItem > bakeup/workshop/fixtures/demo_products.json
+python manage.py tenant_command dumpdata --all --schema=localhost workshop.Product workshop.ProductPrice workshop.ProductHierarchy workshop.Instruction taggit.Tag taggit.TaggedItem > bakeup/workshop/fixtures/demo_products.json
