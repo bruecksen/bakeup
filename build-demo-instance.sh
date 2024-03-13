79d6253b03561d7fe089a389895abdce7fb38a6e
Matthias Brück <hi@brueck.io>
2024-03-13 11:04:26 +1100

    Update build demo server script

--- build-demo-instance.sh
+++ build-demo-instance.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-python manage.py delete_tenant --schema=demo --noinput
-python manage.py create_tenant --noinput --schema_name=demo --name="Bakeup Demo" --is_demo=True --domain-domain=demo.bakeup.org --domain-is_primary=True
-python manage.py tenant_command loaddata --schema=demo bakeup/shop/fixtures/demo_point_of_sale.json
-python manage.py tenant_command loaddata --schema=demo bakeup/users/fixtures/demo_users.json
-python manage.py tenant_command loaddata --schema=demo bakeup/workshop/fixtures/demo_categories.json
-python manage.py tenant_command loaddata --schema=demo bakeup/workshop/fixtures/demo_products.json
-python manage.py create_initial_demo_wagtail_pages --schema=demo --noinput
+/home/django/djangoenv/bin/python /home/django/bakeup/manage.py delete_tenant --schema=demo --noinput --settings=config.settings.production
+/home/django/djangoenv/bin/python /home/django/bakeup/manage.py create_tenant --noinput --schema_name=demo --name="Bakeup Demo" --is_demo=True --domain-domain=demo.bakeup.org --domain-is_primary=True --settings=config.settings.production
+/home/django/djangoenv/bin/python /home/django/bakeup/manage.py tenant_command loaddata --schema=demo bakeup/shop/fixtures/demo_point_of_sale.json --settings=config.settings.production
+/home/django/djangoenv/bin/python /home/django/bakeup/manage.py tenant_command loaddata --schema=demo bakeup/users/fixtures/demo_users.json --settings=config.settings.production
+/home/django/djangoenv/bin/python /home/django/bakeup/manage.py tenant_command loaddata --schema=demo bakeup/workshop/fixtures/demo_categories.json --settings=config.settings.production
+/home/django/djangoenv/bin/python /home/django/bakeup/manage.py tenant_command loaddata --schema=demo bakeup/workshop/fixtures/demo_products.json --settings=config.settings.production
+/home/django/djangoenv/bin/python /home/django/bakeup/manage.py create_initial_demo_wagtail_pages --schema=demo --noinput --settings=config.settings.production
