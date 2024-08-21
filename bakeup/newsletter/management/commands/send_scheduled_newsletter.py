3025520a0da065ee14eb0f2982fe11221b8e1293
Matthias Brück <hi@brueck.io>
2024-08-21 15:56:39 +0200

    Fix management command, adjust templates

--- bakeup/newsletter/management/commands/send_scheduled_newsletter.py
+++ bakeup/newsletter/management/commands/send_scheduled_newsletter.py
@@ -1,27 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
 from django.utils import timezone
-from django_tenants.management.commands import InteractiveTenantOption
+from django_tenants.utils import get_tenant_model
 
 from bakeup.core.tenant_settings import TenantSettings
 from bakeup.newsletter import get_backend
 from bakeup.newsletter.models import CampaignStatus, NewsletterPage
 
 
-class Command(InteractiveTenantOption, BaseCommand):
+class Command(BaseCommand):
     help = "Sends scheduled newsletter"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "-s", "--schema", dest="schema_name", help="specify tenant schema"
-        )
-
     def handle(self, *args, **options):
-        tenant = self.get_tenant_from_options_or_interactive(**options)
-        connection.set_tenant(tenant)
+        tenant = get_tenant_model().objects.get(schema_name=connection.schema_name)
         TenantSettings.overload_settings(tenant)
         scheduled_newsletters = NewsletterPage.objects.filter(
             status=CampaignStatus.SCHEDULED,
